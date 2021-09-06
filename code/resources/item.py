cc254744736bd3e4a817b320e611286155e3f075
Mohit <mohit.agrawal@unthinkable.co>
2021-09-06 18:48:53 +0530

    Added first project using sqlalchemy

---
+++ code/resources/item.py
@@ -0,0 +1,57 @@
+from flask_restful import Resource, reqparse
+from flask_jwt import jwt_required
+from models.item import ItemModel
+
+db_name = 'data.db'
+
+class Item(Resource):
+
+	parser = reqparse.RequestParser() # This will update only the keys which we pass as arguement.
+	parser.add_argument('price',
+		type=float,
+		required=True,
+		help="This field cannot be blank!"
+	)
+	parser.add_argument('store_id',
+		type=int,
+		required=True,
+		help="This field cannot be blank!"
+	)
+
+	@jwt_required() # Now this api needs authentication token to get called.
+	def get(self, name):
+		item = ItemModel.find_by_name(name)
+		if item:
+			return item.json()
+		return {'message': 'Item not found'}, 400 # 404 will return as status code.
+
+	def post(self, name):
+		if ItemModel.find_by_name(name):
+			return {'message': 'Item already exists'}, 404 # 404 will return as status code.
+		data = Item.parser.parse_args()
+		item = ItemModel(name, **data) # **data = data['price'], data['store_id']
+		item.save_to_db()
+
+		return item.json()
+
+	def delete(self, name):
+		item = ItemModel.find_by_name(name)
+		if item is None:
+			return {'message': 'Item not found'}, 404 # 404 will return as status code.
+
+		item.delete_from_db()
+
+	def put(self, name):
+		data = Item.parser.parse_args()
+		item = ItemModel.find_by_name(name)
+		if item is None:
+			item = ItemModel(name, **data)
+		else:		
+			item.price = data['price']
+		item.save_to_db()
+		return item.json()
+
+class ItemList(Resource):
+	def get(self):
+		items = ItemModel.query.all()
+		return {'items': [item.json() for item in items]}
---
+++ code/resources/store.py
@@ -0,0 +1,30 @@
+from flask_restful import Resource
+from models.store import StoreModel
+
+class Store(Resource):
+	def get(self, name):
+		store = StoreModel.find_by_name(name)
+		if store:
+			return {'store': store.json()}
+		return {'message': 'Store not found'}, 400
+
+	def post(self, name):
+		if StoreModel.find_by_name(name):
+			return {'message': 'Store already exists with that name'}, 400
+		store = StoreModel(name)
+		try:
+			store.save_to_db()
+		except Exception:
+			return {'message': 'An error occured while creating the store'}, 500
+		return store.json()
+
+	def delete(self, name):
+		store = StoreModel.find_by_name(name)
+		if store:
+			store.delete_from_db()
+			return {'message': 'Store deleted'}
+		return {'message': 'Store not found'}, 400
+
+class StoreList(Resource):
+	def get(self):
+		return {'stores': [store.json() for store in StoreModel.query.all()]}
